2d4474764c42c2977f49b93192953bc5fda2079f
FarhanXCode7 <74123134+Fxc7@users.noreply.github.com>
2021-06-16 00:46:17 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
 case 'media2url':
-			if ((isMedia && !mek.message.videoMessage || isQuotedImage) && args.length == 0) {
-				const encmedia = isQuotedImage ? JSON.parse(JSON.stringify(mek).replace('quotedM','m')).message.extendedTextMessage.contextInfo : mek
-				const media = await fxc7.downloadAndSaveMediaMessage(encmedia)
-				anu = await uploadFile(media, `${args[0] ? args[0] : Date.now()}.jpg`)
-				reply(anu.result)
-				fs.unlinkSync(media)
-			} else if ((isMedia && mek.message.videoMessage || isQuotedVideo) && args.length == 0) {
-				const encmedia = isQuotedVideo ? JSON.parse(JSON.stringify(mek).replace('quotedM','m')).message.extendedTextMessage.contextInfo : mek
-				const media = await fxc7.downloadAndSaveMediaMessage(encmedia)
-				vid = await uploadFile(media, `${args[0] ? args[0] : Date.now()}.mp4`)
-				reply(mess.wait)
-				reply(vid.result)
-				fs.unlinkSync(media)
-			} else {
-				reply(`Reply Media Dengan Caption ${prefix + command}`)
-			}
-		break
+    const { uploadFile } = require('./upload.js')
+    if ((isMedia && !mek.message.videoMessage || isQuotedImage) && args.length == 0) {
+    const encmedia = isQuotedImage ? JSON.parse(JSON.stringify(mek).replace('quotedM','m')).message.extendedTextMessage.contextInfo : mek
+    const media = await fxc7.downloadAndSaveMediaMessage(encmedia)
+    anu = await uploadFile(media, `${args[0] ? args[0] : Date.now()}.jpg`)
+    reply(anu.result)
+    fs.unlinkSync(media)
+} else if ((isMedia && mek.message.videoMessage || isQuotedVideo) && args.length == 0) {
+    const encmedia = isQuotedVideo ? JSON.parse(JSON.stringify(mek).replace('quotedM','m')).message.extendedTextMessage.contextInfo : mek
+    const media = await fxc7.downloadAndSaveMediaMessage(encmedia)
+    vid = await uploadFile(media, `${args[0] ? args[0] : Date.now()}.mp4`)
+    reply(mess.wait)
+    reply(vid.result)
+    fs.unlinkSync(media)
+} else {
+    reply(`Reply Media Dengan Caption ${prefix + command}`)
+	}
+break
 
 //Sesuaikan Dengan Script Kalian
